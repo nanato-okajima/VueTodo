35bfe834e030726f8fb9a8d661a1c4930d1aaa21
n_nakajima <n_nakajima@fujiball.co.jp>
2021-06-21 10:16:12 +0900

    initial commit

---
+++ client/src/store/auth.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const state = {
+
+}
+
+const getters = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
+}
